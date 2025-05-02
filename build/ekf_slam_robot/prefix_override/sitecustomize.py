d9393d3ebb8d3cb4820abc28fab2b13a308e965a
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2025-05-02 17:06:22 +0300

    Delete build directory

--- build/ekf_slam_robot/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/kseikoff/ros2_ws/src/ekf_slam_robot/install/ekf_slam_robot'
--- build/ekf_slam_robot/share/ekf_slam_robot/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/kseikoff/ros2_ws/src/ekf_slam_robot/build/ekf_slam_robot"
